2f21ba3bbc88a03aac7bf88ae50fb4d63a7985e3
yauhenit <yauhenit@playtika.com>
2025-10-13 16:15:56 +0200

    Completed React-HW-1

---
+++ react-homework-1/src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.css'
+import PhoneList from './components/PhoneList'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>React-HW-1</h1>
+      <PhoneList />
+    </div>
+  )
+}
